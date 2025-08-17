37921df1a2dbe749d5c0b421198dbd2f4452bbd7
Junghwan John Goh <jhgoh@cern.ch>
2025-08-18 08:13:11 +0900

    refactor: apply snake case and update winston name

--- makeShapes.py
+++ makeShapes.py
@@ -2,10 +2,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def makePlanar(din, dout, angle, width=1200, height=1200):
+
+def make_planar(din, dout, angle, width=1200, height=1200):
   result = {
     'name': 'planar',
-    'nwalls': 4,
+    'n_walls': 4,
     'din': din,
     'dout': dout,
     'angle': angle,
@@ -13,69 +14,68 @@ def makePlanar(din, dout, angle, width=1200, height=1200):
   }
 
   ## Left and right walls
-  result['shapes'].append({'x':[-width/2, -width/2], 'y':[0, height]})
-  result['shapes'].append({'x':[width/2, width/2], 'y':[0, height]})
+  result['shapes'].append({'x': [-width / 2, -width / 2], 'y': [0, height]})
+  result['shapes'].append({'x': [width / 2, width / 2], 'y': [0, height]})
 
   ## Main mirrors
-  h = dout*np.tan(np.deg2rad(angle))
-  result['shapes'].append({'x':[dout/2, din/2], 'y':[0, h]})
-  result['shapes'].append({'x':[-dout/2, -din/2], 'y':[0, h]})
+  h = dout * np.tan(np.deg2rad(angle))
+  result['shapes'].append({'x': [dout / 2, din / 2], 'y': [0, h]})
+  result['shapes'].append({'x': [-dout / 2, -din / 2], 'y': [0, h]})
 
   return result
 
-def makeWinston(din, dout, critAngle, width=1200, height=1200, nPoint=25):
+
+def make_winston(din, dout, crit_angle, width=1200, height=1200, n_points=25):
   result = {
-    'name': 'planar',
-    'nwalls': 4,
+    'name': 'winston',
+    'n_walls': 4,
     'din': din,
     'dout': dout,
-    'critAngle': critAngle,
+    'crit_angle': crit_angle,
     'shapes': [],
   }
 
   ## Left and right walls
-  result['shapes'].append({'x':[-width/2, -width/2], 'y':[0, height]})
-  result['shapes'].append({'x':[width/2, width/2], 'y':[0, height]})
+  result['shapes'].append({'x': [-width / 2, -width / 2], 'y': [0, height]})
+  result['shapes'].append({'x': [width / 2, width / 2], 'y': [0, height]})
 
   ## Main mirrors
-  critAngle = np.deg2rad(critAngle)
-  rin = din/2
-  rout = dout/2
-  sinC, cosC = np.sin(critAngle), np.cos(critAngle)
-  l = (rin + rout) / np.tan(critAngle) ## Intrinsic height
-  f = rout*(1+sinC) ## Focal length
+  crit_angle = np.deg2rad(crit_angle)
+  r_in = din / 2
+  r_out = dout / 2
+  sin_c, cos_c = np.sin(crit_angle), np.cos(crit_angle)
+  intrinsic_height = (r_in + r_out) / np.tan(crit_angle)
+  focal_length = r_out * (1 + sin_c)
 
   ## Build parabola in mirror axis
-  d1 = 2*rout*cosC
-  d2 = l*np.sin(2*critAngle)/cosC
-  xr = np.linspace(d1, d2, nPoint)
-  yr = xr**2/4/f
+  d_1 = 2 * r_out * cos_c
+  d_2 = intrinsic_height * np.sin(2 * crit_angle) / cos_c
+  x_vals = np.linspace(d_1, d_2, n_points)
+  y_vals = x_vals ** 2 / 4 / focal_length
 
   ## Rotate & translate the mirror
-  xx = xr*cosC - (yr-f)*sinC - rout
-  yy = xr*sinC + (yr-f)*cosC
+  x_coords = x_vals * cos_c - (y_vals - focal_length) * sin_c - r_out
+  y_coords = x_vals * sin_c + (y_vals - focal_length) * cos_c
 
-  #yy = yy[xx<=rout]
-  #xx = xx[xx<=rout]
-
-  result['shapes'].append({'x':xx, 'y':yy})
-  result['shapes'].append({'x':-xx, 'y':yy})
+  result['shapes'].append({'x': x_coords, 'y': y_coords})
+  result['shapes'].append({'x': -x_coords, 'y': y_coords})
 
   return result
 
 def propagate(x, y, vx, vy, mirrors, nstep=10000):
   for i in range(nstep):
-    
+    pass
+
 
 if __name__ == '__main__':
   par_din = 1200
   par_dout = 500
   par_angle = 25
-  #par_angle = 10
+  # par_angle = 10
 
-  #result = makePlanar(par_din, par_dout, par_angle)
-  result = makeWinston(par_din, par_dout, par_angle)
-  #print(result)
+  # result = make_planar(par_din, par_dout, par_angle)
+  result = make_winston(par_din, par_dout, par_angle)
+  # print(result)
 
   rmax = 0
   for shape in result['shapes']:
@@ -86,11 +86,11 @@ if __name__ == '__main__':
     rmax = max(np.hypot(x, y).max(), rmax)
 
   ## Draw axis
-  plt.plot([-result['din']/2, result['din']/2], [0, 0], '-.k', linewidth=0.5)
-  plt.plot([0, 0], [0, 1.5*rmax], '-.k', linewidth=0.5)
+  plt.plot([-result['din'] / 2, result['din'] / 2], [0, 0], '-.k', linewidth=0.5)
+  plt.plot([0, 0], [0, 1.5 * rmax], '-.k', linewidth=0.5)
 
   plt.xlim(-rmax, rmax)
-  plt.ylim(-0.2*rmax, 1.2*rmax)
+  plt.ylim(-0.2 * rmax, 1.2 * rmax)
   plt.gca().set_aspect('equal', adjustable='box')
   plt.show()
 
